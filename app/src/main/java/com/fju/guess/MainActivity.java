02bf8cd6c88b6f79ab067680e4f68f097537d707
Huang huai jung <je3rjoy@gmail.com>
2017-11-11 23:58:41 +0800

    v2

--- app/src/main/java/com/fju/guess/MainActivity.java
+++ app/src/main/java/com/fju/guess/MainActivity.java
@@ -26,7 +26,11 @@ public class MainActivity extends AppCompatActivity {
         }else if(guess > n){
                 Toast.makeText(this, "小一點", Toast.LENGTH_SHORT).show();
                 }else if(guess == n){
-                   Toast.makeText(this, "答對了!!!", Toast.LENGTH_LONG).show();
+                   new AlertDialog.Builder(this)
+                            .setTitle("恭喜!!!")
+                            .setMessage("答對了!!")
+                            .setPositiveButton("OK", null)
+                            .show();
 
 
         }
